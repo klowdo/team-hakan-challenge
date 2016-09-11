2b86212e98c590ae35efd50ad04a05f4c5d22a82
Mats Iremark <mats.x.iremark@collectorbank.se>
2016-09-11 19:16:22 +0200

    Added Mats solution. Changed folder structure.

---
+++ v35/Mats/Tullvakt.Test/UnitTest1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tullvakt.Test
+{
+
+    public class UnitTest1
+
+    {
+        private static Car LattMotorcykel;
+        private static Car TungMotorcykel;
+        private static Car LattPersonbil;
+        private static Car TungPersonbil;
+        private static Car LattLastbil;
+        private static Car TungLastbil;
+
+
+        [SetUp]
+        public void InitCars()
+        {
+            LattLastbil = new Car
+            {
+                Miljobil = false,
+                Typ = Car.Type.Lastbil,
+                Weight = 999
+            };
+            TungLastbil = new Car
+            {
+                Miljobil = false,
+                Typ = Car.Type.Lastbil,
+                Weight = 1001
+            };
+            LattMotorcykel = new Car
+            {
+                Miljobil = false,
+                Typ = Car.Type.Motorcykel,
+                Weight = 999
+            };
+            TungMotorcykel = new Car
+            {
+                Miljobil = false,
+                Typ = Car.Type.Motorcykel,
+                Weight = 1001
+            };
+            LattPersonbil = new Car
+            {
+                Miljobil = false,
+                Typ = Car.Type.Personbil,
+                Weight = 999
+            };
+            TungPersonbil = new Car
+            {
+                Miljobil = false,
+                Typ = Car.Type.Personbil,
+                Weight = 1001
+            };
+
+        }
+
+        [TestCase()]
+        public void Rule1(Car car)
+        {
+            var tullvakt = new Tullvakt();
+            
+            Assert.AreEqual(0,tullvakt.ApplyRule1(0,LattMotorcykel));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(0, LattLastbil));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(0, LattPersonbil));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(tullvakt.BasePriceHigh, TungMotorcykel));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(tullvakt.BasePriceHigh, TungLastbil));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(tullvakt.BasePriceHigh, TungPersonbil));
+        }
+
+        [Test]
+        public void Rule2()
+        {
+            var tullvakt = new Tullvakt();
+            Assert.AreEqual(0, tullvakt.ApplyRule1(tullvakt.BasePriceLow, LattMotorcykel));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(tullvakt.BasePriceLow, LattLastbil));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(tullvakt.BasePriceLow, LattPersonbil));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(0, TungMotorcykel));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(0, TungLastbil));
+            Assert.AreEqual(0, tullvakt.ApplyRule1(0, TungPersonbil));
+        }
+
+        [Test]
+        public void Rule3()
+        {
+            var tullvakt = new Tullvakt();
+
+            double basePrice = 1000;
+
+            var date1 = new DateTime(2016,9,3,18,1,0); //helg 18.01
+            var date2 = new DateTime(2016, 9, 3, 17, 59, 0); //helg 17.59
+            var date3 = new DateTime(2016, 9, 3, 6, 0, 0); //helg 6.00
+            var date4 = new DateTime(2016, 9, 3, 5, 59, 0); //helg 5.59
+
+            var date5 = new DateTime(2016, 9, 2, 18, 1, 0); //helg 18.01
+            var date6 = new DateTime(2016, 9, 2, 17, 59, 0); //helg 17.59
+            var date7 = new DateTime(2016, 9, 2, 6, 0, 0); //helg 6.00
+            var date8 = new DateTime(2016, 9, 2, 5, 59, 0); //helg 5.59
+            
+
+            Assert.AreEqual(basePrice,tullvakt.ApplyRule3(basePrice,date1));
+            Assert.AreEqual(basePrice, tullvakt.ApplyRule3(basePrice, date2));
+            Assert.AreEqual(basePrice, tullvakt.ApplyRule3(basePrice, date3));
+            Assert.AreEqual(basePrice, tullvakt.ApplyRule3(basePrice, date4));
+            Assert.AreEqual(basePrice*0.5, tullvakt.ApplyRule3(basePrice, date5));
+            Assert.AreEqual(basePrice, tullvakt.ApplyRule3(basePrice, date6));
+            Assert.AreEqual(basePrice, tullvakt.ApplyRule3(basePrice, date7));
+            Assert.AreEqual(basePrice*0.5, tullvakt.ApplyRule3(basePrice, date8));
+
+        }
+
+
+        [Test]
+        public void Rule4()
+        {
+            var tullvakt = new Tullvakt();
+            double baseprice = 666;
+            Assert.AreEqual(baseprice, tullvakt.ApplyRule1(tullvakt.BasePriceLow, LattMotorcykel));
+            Assert.AreEqual(baseprice, tullvakt.ApplyRule1(tullvakt.BasePriceLow, LattLastbil));
+            Assert.AreEqual(baseprice, tullvakt.ApplyRule1(tullvakt.BasePriceLow, LattPersonbil));
+            Assert.AreEqual(baseprice, tullvakt.ApplyRule1(0, TungMotorcykel));
+            Assert.AreEqual(baseprice, tullvakt.ApplyRule1(0, TungLastbil));
+            Assert.AreEqual(baseprice, tullvakt.ApplyRule1(0, TungPersonbil));
+        }
+        [Test]
+        public void Rule5()
+        {
+        }
+        [Test]
+        public void Rule6()
+        {
+        }
+        [Test]
+        public void Rule7()
+        {
+        }
+        [Test]
+        public void Rule8()
+        {
+        }
+    }
+}
---
+++ v35/Mats/Tullvakt/Tullvakt.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tullvakt
+{
+    public class Tullvakt
+    {
+        public double BasePriceHigh = 1000;
+        public double BasePriceLow = 500;
+        
+
+        public double Price(Car car, DateTime time)
+        {
+            var price = 0.0;
+            price = ApplyRule1(price, car);
+            price = ApplyRule2(price, car);
+            price = ApplyRule3(price, time);
+            price = ApplyRule4(price, car);
+            price = ApplyRule6(price, car);
+            price = ApplyRule7(price, time);
+            price = ApplyRule8(price, car);
+            return price;
+        }
+
+        private static bool IsWeekend(DateTime time)
+        {
+            return (time.DayOfWeek == DayOfWeek.Sunday || time.DayOfWeek == DayOfWeek.Saturday);
+        }
+
+        public double ApplyRule1(double price, Car car)
+        {
+            return car.Weight > 1000 ? BasePriceHigh : price;
+        }
+
+        public double ApplyRule2(double price, Car car)
+        {
+            return car.Weight < 1000 ? BasePriceLow : price;
+        }
+
+        public double ApplyRule3(double price, DateTime time)
+        {
+            if (!(IsWeekend(time)) && (time.Hour > 17 || time.Hour < 6))
+                return price * 0.5;
+            return price;
+        }
+
+        public double ApplyRule4(double price, Car car)
+        {
+            if (car.Typ == Car.Type.Lastbil) return 2000;
+            return price;
+        }
+
+        public double ApplyRule6(double price, Car car)
+        {
+            if (car.Typ == Car.Type.Motorcykel) return price*0.7;
+            return price;
+        }
+
+        public double ApplyRule7(double price, DateTime time)
+        {
+            if (IsWeekend(time)) return price*2;
+            return price;
+        }
+
+        public double ApplyRule8(double price, Car car)
+        {
+            if (car.Miljobil) return 0;
+            return price;
+        }
+    }
+
+    public class Car
+    {
+        public enum Type
+        {
+            Lastbil,
+            Motorcykel,
+            Personbil
+        };
+
+        public Type Typ;
+        public int Weight;
+        public bool Miljobil;
+
+    }
+
+
+}
+
